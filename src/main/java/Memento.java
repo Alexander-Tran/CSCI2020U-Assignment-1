16f900f21949ed0694e4325d85c893d0592296a6
Alexander Tran <alexander.tran@ontariotechu.net>
2022-02-21 22:19:46 -0500

    Added Memento Files

--- src/main/java/Memento.java
+++ src/main/java/Memento.java
@@ -1,2 +1,11 @@
 public class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
